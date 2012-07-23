7b36b1dd6630ae1bdc9691a71978967660c9ea42
Mike Wielgosz <dev.mwielgosz@gmail.com>
2012-07-23 18:08:26 -0400

    Merge commit 'android-4.1.1_r3' into jellybean
    Conflicts:
    	full_crespo4g.mk
    	init.herring.rc
    
    Change-Id: I99dda6724a647195b8a5928533b34c2e4ec54ff3

--- factory-images/generate-factory-images-package.sh
+++ factory-images/generate-factory-images-package.sh
@@ -1,5 +1,6 @@
-#
-# Copyright (C) 2008 The Android Open Source Project
+#!/bin/sh
+
+# Copyright (C) 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,12 +13,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# This file is executed by build/envsetup.sh, and can use anything
-# defined in envsetup.sh.
-#
-# In particular, you can add lunch options with the add_lunch_combo
-# function: add_lunch_combo generic-eng
+# 185293 = GWK74
+# 299849 = IMM76D
 
-add_lunch_combo full_crespo4g-userdebug
+PRODUCT=sojus
+DEVICE=crespo4g
+BUILD=299849
+VERSION=imm76d
+BOOTLOADER=d720sprke1
+BOOTLOADERSRC=bootloader.pr.img
+RADIO=d720sprlc1
+RADIOSRC=radio.pr.img
+
+source ../../../common/generate-factory-images-common.sh
